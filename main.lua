166fa395749a02802b4149ead6b75f9691da28dc
Daniel Blackburn <craklyn@gmail.com>
2016-11-22 15:14:55 -0800

    example curl command from lua

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+local socket = require("socket")
+
 function love.load()
 --  texas = love.graphics.newImage("texas-outline.png")
   texas = love.graphics.newImage("texas-transparent.png")
@@ -7,11 +9,20 @@ function love.load()
   texasSx = 1.0
   texasSy = 1.0
   dt = love.timer.getDelta
+
+  if not love.filesystem.exists("expo.png") then
+    local http = require("socket.http")
+
+    local b, c, h = http.request("http://love2d.org/w/images/e/e3/Resource-AnalExplosion.png")
+    love.filesystem.write("expo.png", b)
+  end
+  texas = love.graphics.newImage("expo.png")
+
 end
 
 function love.update(dt)
-  texasX = texasX + dt*0.5
-  texasY = texasY + dt*0.8
+  texasX = texasX + dt*2.5
+  texasY = texasY + dt+0.8
   texasR = texasR - dt*0.1
   texasSx = texasSx * (1 - dt*0.1)
   texasSy = texasSy * (1 - dt*0.1)
