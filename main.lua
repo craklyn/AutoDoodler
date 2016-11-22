44f3768666a567f3f96d76c8e7ad35b3d6b2b397
Daniel Blackburn <craklyn@gmail.com>
2016-11-22 13:21:46 -0800

    Basic texas drawing.  Woot woot

--- main.lua
+++ main.lua
@@ -1,5 +1,19 @@
 function love.load()
   texas = love.graphics.newImage("texas-outline.png")
+  texasX = 100
+  texasY = 100
+  texasR = 0
+  texasSx = 1.0
+  texasSy = 1.0
+  dt = love.timer.getDelta
+end
+
+function love.update(dt)
+  texasX = texasX + dt*0.5
+  texasY = texasY + dt*0.8
+  texasR = texasR - dt*0.1
+  texasSx = texasSx * (1 - dt*0.1)
+  texasSy = texasSy * (1 - dt*0.1)
 end
 
 
@@ -7,6 +21,6 @@ function love.draw()
   love.graphics.print('Hello World!', 400, 300)
 --    love.graphics.draw( drawable, x, y, r, sx, sy, ox, oy, kx, ky )
   love.graphics.draw( texas, 500, 500, 0, 0, 0, 0, 0, 0, 0 )
-  love.graphics.draw(texas, 100, 100)
+  love.graphics.draw(texas, texasX, texasY, texasR, texasSx, texasSy)
 end
 
