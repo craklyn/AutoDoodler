8d058b960728d7374bfc64a741e1f03f5e7eca49
Daniel Blackburn <craklyn@gmail.com>
2016-11-22 12:06:23 -0800

    Updated code

--- main.lua
+++ main.lua
@@ -1,4 +1,12 @@
+function love.load()
+  texas = love.graphics.newImage("texas-outline.png")
+end
+
+
 function love.draw()
-    love.graphics.print('Hello World!', 400, 300)
+  love.graphics.print('Hello World!', 400, 300)
+--    love.graphics.draw( drawable, x, y, r, sx, sy, ox, oy, kx, ky )
+  love.graphics.draw( texas, 500, 500, 0, 0, 0, 0, 0, 0, 0 )
+  love.graphics.draw(texas, 100, 100)
 end
 
